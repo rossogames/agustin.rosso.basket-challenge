c82a2ba1669df25f753e9d57e38056c9c66dc446
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-07-24 09:27:20 +0200

    Se agrega rebote a la pelota

--- basket/Assets/Scripts/Gameplay/Components/TargetTrigger.cs
+++ basket/Assets/Scripts/Gameplay/Components/TargetTrigger.cs
@@ -4,6 +4,15 @@ namespace Basket.Gameplay.Components
 {
     public class TargetTrigger : MonoBehaviour
     {
+        [SerializeField]
+        private int targetIndex;
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Ball"))
+            {
+                Debug.LogWarning($"test {targetIndex}");
+            }
+        }
     }
 }
