2d79ee0c1547e03f9a4547a9cb390164989bf660
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-07-26 21:03:06 +0200

    implementacion preliminar de aim bar

--- basket/Assets/Scripts/UI/AimBar.cs
+++ basket/Assets/Scripts/UI/AimBar.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 
 namespace Basket.UI
 {
-    public class ShootLine : MonoBehaviour, IEventListener<InputDragEvent>
+    public class AimBar : MonoBehaviour, IEventListener<InputDragEvent>
     {
         private IEventService _eventService;
 
         [SerializeField]
-        private Image _lineImage;
+        private RectTransform _transformInnerBackground;
+
+        [SerializeField]
+        private Image _imageFiller;
 
         private void Awake()
         {
@@ -30,12 +33,7 @@ namespace Basket.UI
         public void OnEventInvoked(InputDragEvent eventArg)
         {
             var distance = eventArg.EndScreenPos - eventArg.StartScreenPos;
-
-            _lineImage.transform.position = eventArg.StartScreenPos;
-            _lineImage.rectTransform.sizeDelta = new Vector2(15, distance.magnitude);
-
-            float angle = Mathf.Atan2(distance.y, distance.x) * Mathf.Rad2Deg;
-            _lineImage.transform.rotation = Quaternion.Euler(0, 0, angle - 90);
+            _imageFiller.fillAmount = distance.magnitude / _transformInnerBackground.rect.height;
         }
     }
 }
